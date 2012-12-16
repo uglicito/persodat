0d7c71e5cc872e8c810623243adecc36f23f4f4b
user <user@pc>
2012-12-16 02:28:00 +0100

    array in arbeit

--- Persodat/src/Person.java
+++ Persodat/src/Person.java
@@ -30,33 +30,35 @@ public class Person{
 	public void setPerson(){
 		
 		int b;
+		int c;
 		int i;
-		
+				
 		String[][] persMatrix;
 		persMatrix = new String[5][3];
 		
 		for (i=1; i < persMatrix.length; i++){
-			if (i!=1){
+			if (i!=1)
+			{
 				i=i-1;
 				System.out.println(i+" "+persMatrix[i][0]+" "+persMatrix[i][1]+" "+persMatrix[i][2]);
 				System.out.println("Sind die Eingaben Korrekt?");
+				c=StdInput.readInt("Geben Sie bitte 1 für Ja und 0 für Nein ein");
 			}			
+				
 				System.out.println("Möchten Sie eine weitere Person Aufnehmen?");
-				b=StdInput.readInt("Geben Sie bitte "+1+" für Ja und "+0+" für Nein ein");
-			if (b==1){
-					persMatrix[i][0]	=StdInput.readString("Name    : ");
-					persMatrix[i][1]	=StdInput.readString("Vorname : ");
-					persMatrix[i][2] 	=StdInput.readString("E-Mail  : ");
+				b=StdInput.readInt("Geben Sie bitte 1 für Ja und 0 für Nein ein");
+			 	if (b==1){
+						persMatrix[i][0]	=StdInput.readString("Name    : ");
+						persMatrix[i][1]	=StdInput.readString("Vorname : ");
+						persMatrix[i][2] 	=StdInput.readString("E-Mail  : ");
+				}
+				else{
+							System.out.println(i+" "+persMatrix[i][0]+" "+persMatrix[i][1]+" "+persMatrix[i][2]);
+							break;
+				}
 			}
-			else{
-				System.out.println(i+" "+persMatrix[i][0]+" "+persMatrix[i][1]+" "+persMatrix[i][2]);
-				break;
-			}
-	
 		}
-			
 	}
-}	
 //	/*void setName(String neuName)
 //	{
 //		name = neuName;
