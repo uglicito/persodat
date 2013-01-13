f22560b59e802778e1c3df769dde025582d7c5b8
user <user@pc>
2013-01-13 23:42:23 +0100

    nur 4 Personen

--- Persodat/src/Main.java
+++ Persodat/src/Main.java
@@ -2,33 +2,33 @@
 public class Main{
 		public static void main(String[]args){
 	
-	int wahl;
+			int wahl;
 	
-	for (;;) {
-		System.out.println("		Persodat - Kontaktverwaltung");
-		System.out.println("Wollen Sie...");
-		System.out.println();
-		System.out.println("   eine neue Person aufnehmen >1");
-		System.out.println("            Records auflisten >2");
-		System.out.println("Records in eine Datei sichern >3");
-		System.out.println("Records aus einer Datei laden >4");
-		System.out.println("                Datei Löschen >5");
-		System.out.println("       das Programm verlassen >6");
-		System.out.println();
-		wahl=StdInput.readInt("Ihre Eingabe von 1-6: ");
+			for (;;) {
+				System.out.println("		Persodat - Kontaktverwaltung");
+				System.out.println("Wollen Sie...");
+				System.out.println();
+				System.out.println("   eine neue Person aufnehmen >1");
+				System.out.println("            Records auflisten >2");
+				System.out.println("Records in eine Datei sichern >3");
+				System.out.println("Records aus einer Datei laden >4");
+				System.out.println("                Datei Löschen >5");
+				System.out.println("       das Programm verlassen >6");
+				System.out.println();
+				wahl=StdInput.readInt("Ihre Eingabe von 1-6: ");
 		
-		//while (wahl>6 || wahl<1);
-		switch (wahl)
-		{
-			case 1:	Person neu = new Person(); 
-					neu.setPerson(); break;
+				//	while (wahl>6 || wahl<1);
+				switch (wahl)
+				{
+				case 1:	Person neu = new Person(); 
+						neu.setPerson(); break;
 		
 //		case 2:  System.out.println("Gut");break;
 //		case 3:  System.out.println("Befriedigend");break;
 //		case 4:  System.out.println("Ausreichend");break;
 //		case 5:  System.out.println("Mangelhaft");break;
 //		case 6:  System.out.println("Ungenügend");break;
-		}
-	}
+				}
+			}
   }
 }
--- Persodat/src/Person.java
+++ Persodat/src/Person.java
@@ -1,80 +1,42 @@
 public class Person{
-	/*
-	  String name;
-	String vorname;
-	String email;
-	
-	
-	public void setName(String name)
-	{
-		this.name=name;
-	}
-	public void setVorname(String vorname)
-	{
-		this.vorname=vorname;
-	}
-	public void setEmail(String email)
-	{
-		this.email=email;
-	}
-	String getName(){
-		return name;
-	}
-	String getVorname(){
-		return vorname;
-	}
-	String getEmail(){
-		return email;
-	}   
-	 */
 	public void setPerson(){
 		
-		int b;
-		int c;
-		int i;
-				
-		String[][] persMatrix;
-		persMatrix = new String[5][3];
+		int c = 0;
+		int d = 0;
+		int i = 0;
+		int z;
 		
-		for (i=1; i < persMatrix.length; i++){
-			if (i!=1)
-			{
-				i=i-1;
-				System.out.println(i+" "+persMatrix[i][0]+" "+persMatrix[i][1]+" "+persMatrix[i][2]);
-				System.out.println("Sind die Eingaben Korrekt?");
-				c=StdInput.readInt("Geben Sie bitte 1 für Ja und 0 für Nein ein");
-			}			
+		String[][] persMatrix;
+		persMatrix = new String[5][5];
+		
+		for (i=1; i <= 6; i++){
+			
+				persMatrix[i][0]	=StdInput.readString("Name    : ");
+				persMatrix[i][1]	=StdInput.readString("Vorname : ");
+				persMatrix[i][2] 	=StdInput.readString("E-Mail  : ");
 				
-				System.out.println("Möchten Sie eine weitere Person Aufnehmen?");
-				b=StdInput.readInt("Geben Sie bitte 1 für Ja und 0 für Nein ein");
-			 	if (b==1){
-					persMatrix[i][0]	=StdInput.readString("Name    : ");
-					persMatrix[i][1]	=StdInput.readString("Vorname : ");
-					persMatrix[i][2] 	=StdInput.readString("E-Mail  : ");
+				System.out.println("PersNr: "+i+" "+persMatrix[i][0]+" "+persMatrix[i][1]+" "+persMatrix[i][2]);
+				System.out.println("Sind die Eingaben Korrekt?");
+				c=StdInput.readInt("Geben Sie bitte 1 für Ja und 0 für Nein ein.");
+				
+				if (c==0){
+					i--;
+					continue;
+				}
+				
+				else if (i!=6){
+					z=6-i;
+					System.out.println("Möchten Sie eine weitere Person Aufnehmen?");
+					System.out.println("Es koennen noch "+z+" weitere Personen aufgenommen werden.");
+					d=StdInput.readInt("Geben Sie bitte 1 für Ja und 0 für Nein ein.");
+				}
+				
+				if (d==1){
+					continue;
 				}
 				else{
-					System.out.println(i+" "+persMatrix[i][0]+" "+persMatrix[i][1]+" "+persMatrix[i][2]);
 					break;
 				}
-			}
 		}
-	}
-//	/*void setName(String neuName)
-//	{
-//		name = neuName;
-//	}
-//	void setVorname(String neuVorname)
-//	{
-//		name = neuVorname;
-//	}
-//	void setEmail(String neuEmail)
-//	{
-//		name = neuEmail;
-//	}
-//	*/
-//public void setPerson(String name, String vorname, String email)
-//{
-//	this.name=name;
-//	this.vorname=name;
-//	this.email=name;
-//}
+ }
+}
